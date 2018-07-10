888c447d94d120315ec8516cd2394bb6648c9341
zegodev <dev@zego.im>
2018-07-10 16:05:07 +0800

    Update ZegoApiManager.java

--- android/LiveDemo5/app/src/main/java/com/zego/livedemo5/ZegoApiManager.java
+++ android/LiveDemo5/app/src/main/java/com/zego/livedemo5/ZegoApiManager.java
@@ -156,26 +156,22 @@ public class ZegoApiManager {
      * @param appId
      * @return
      */
+    
+    /** Please contact ZEGO support to get the AppID and signKey */
+    /** 请开发者联系 ZEGO support 获取 APPID 与 key */
     private byte[] requestSignKey(long appId) {
         if (isRtmpProduct(appId)) {
-            return new byte[] {(byte) 0x91, (byte) 0x93, (byte) 0xcc, (byte) 0x66, (byte) 0x2a, (byte) 0x1c, (byte) 0x0e, (byte) 0xc1,
-                    (byte) 0x35, (byte) 0xec, (byte) 0x71, (byte) 0xfb, (byte) 0x07, (byte) 0x19, (byte) 0x4b, (byte) 0x38,
-                    (byte) 0x41, (byte) 0xd4, (byte) 0xad, (byte) 0x83, (byte) 0x78, (byte) 0xf2, (byte) 0x59, (byte) 0x90,
-                    (byte) 0xe0, (byte) 0xa4, (byte) 0x0c, (byte) 0x7f, (byte) 0xf4, (byte) 0x28, (byte) 0x41, (byte) 0xf7
+            return new byte[] {
             };
         } else if (isUdpProduct(appId)) {
-            return new byte[] {(byte)0x1e, (byte)0xc3, (byte)0xf8, (byte)0x5c, (byte)0xb2, (byte)0xf2, (byte)0x13, (byte)0x70,
-                    (byte)0x26, (byte)0x4e, (byte)0xb3, (byte)0x71, (byte)0xc8, (byte)0xc6, (byte)0x5c, (byte)0xa3,
-                    (byte)0x7f, (byte)0xa3, (byte)0x3b, (byte)0x9d, (byte)0xef, (byte)0xef, (byte)0x2a, (byte)0x85,
-                    (byte)0xe0, (byte)0xc8, (byte)0x99, (byte)0xae, (byte)0x82, (byte)0xc0, (byte)0xf6, (byte)0xf8};
+            return new byte[] {
+            };
         } else if (isInternationalProduct(appId)) {
-            return new byte[] {(byte)0x5d, (byte)0xe6, (byte)0x83, (byte)0xac, (byte)0xa4, (byte)0xe5, (byte)0xad, (byte)0x43,
-                    (byte)0xe5, (byte)0xea, (byte)0xe3, (byte)0x70, (byte)0x6b, (byte)0xe0, (byte)0x77, (byte)0xa4,
-                    (byte)0x18, (byte)0x79, (byte)0x38, (byte)0x31, (byte)0x2e, (byte)0xcc, (byte)0x17, (byte)0x19,
-                    (byte)0x32, (byte)0xd2, (byte)0xfe, (byte)0x22, (byte)0x5b, (byte)0x6b, (byte)0x2b, (byte)0x2f};
+            return new byte[] {
+            };
         }
 
-        throw new IllegalArgumentException(String.format("appId: <%d> is illegal, must both 1 or 1739272706", appId));
+        throw new IllegalArgumentException(String.format("appId: <%d> is illegal", appId));
     }
 
     /**
@@ -279,19 +275,21 @@ public class ZegoApiManager {
         mEnableAudioPrep = enableAudioPrep;
     }
 
+    /** Please contact ZEGO support to get the AppID and signKey */
+    /** 请开发者联系 ZEGO support 获取 APPID 与 key */
     public long getAppID() {
         return mAppID;
     }
 
     public boolean isInternationalProduct(long appId) {
-        return appId == 3322882036L;
+        return appId == 100L;
     }
 
     public boolean isUdpProduct(long appId) {
-        return appId == 1739272706L;
+        return appId == 10L;
     }
 
     public boolean isRtmpProduct(long appId) {
-        return appId == 1L;
+        return appId == 0L;
     }
 }
